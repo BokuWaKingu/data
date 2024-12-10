ebab639e2696328721e6a1dd06b55240766ce1c5
BokuWaKingu <156182597+BokuWaKingu@users.noreply.github.com>
2024-12-10 16:03:26 +0100

    login vite fait

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,28 +1,42 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import logo from "/logo1.png"
+import { CardFooter } from "@/components/ui/card"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="flex items-center justify-center">
       <Card className="w-full max-w-md">
         <CardHeader>
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
-          <CardDescription>Enter your credentials to access your account</CardDescription>
+          <img src={logo} alt="Logo" width="300" height="100" />
         </CardHeader>
         <CardContent>
           <form className="space-y-4">
             <div className="space-y-2">
               <label htmlFor="email" className="text-sm font-medium">Email</label>
-              <Input id="email" type="email" placeholder="Enter your email" required />
+              <Input id="email" type="email" placeholder="Addresse email" required />
             </div>
             <div className="space-y-2">
               <label htmlFor="password" className="text-sm font-medium">Password</label>
-              <Input id="password" type="password" placeholder="Enter your password" required />
+              <Input id="password" type="password" placeholder="Mot de passe" required />
             </div>
-            <Button type="submit" className="w-full">Login</Button>
+            <Button type="submit" className="w-full bg-orange-500 text-white">Connexion</Button>
           </form>
         </CardContent>
+        <CardFooter className="flex justify-between items-center">
+        <Button 
+            variant="outline" 
+            className="w-full"
+          >
+            <img 
+              src="/microsoft.png"
+              alt="Microsoft Logo" 
+              className="h-7 w-7 object-contain"
+            />
+            {'Connect with Microsoft'}
+          </Button>
+        </CardFooter>
       </Card>
     </div>
   )
